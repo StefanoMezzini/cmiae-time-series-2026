e8e1b37938153a4616d8ce1d5f49280bdeff8881
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-11-19 13:58:48 -0600

    almost finished day 3

---
+++ day-3/day-3.R
@@ -0,0 +1,217 @@
+#' day 3: 2026-02-10
+
+# attach necessary packages
+library('dplyr')     # for data wrangling
+library('tidyr')     # for data wrangling
+library('ggplot2')   # for fancy plot
+library('mgcv')      # for modeling
+library('gratia')    # for plotting models
+library('cowplot')   # for multi-panel plots
+library('khroma')    # for color palettes
+theme_set(theme_bw())
+
+#' import the `co2` dataset and change to a format usable by `gam()`
+d_co2 <- tibble(dec_date = as.vector(time(co2)),
+                year = floor(dec_date),
+                season = dec_date - year,
+                co2_ppm = as.vector(co2))
+d_co2
+
+ggplot(d_co2, aes(dec_date, co2_ppm)) + geom_point(alpha = 0.3)
+
+# fit a GAM as an example
+#' `log(mu) = b_0 + f_1(year) + f_2(season) + f_3(year, season)`
+#' `mu = exp( b_0 + f_1(year) + f_2(season) + f_3(year, season) )`
+#' `= exp(b_0) * exp(f_1(year)) * exp(f_2(season)) * exp(f_3(year, season))`
+m_co2 <- gam(co2_ppm ~
+               # long-term trend
+               s(year, k = 10) +
+               # seasonal trend
+               s(season, bs = 'cc', k = 10) +
+               # change in seasonal trend over the years
+               ti(year, season, bs = c('cr', 'cc'), k = 5),
+             data = d_co2, family = Gamma(link = 'log'), method = 'REML',
+             knots = list(season = c(0, 1))) # force 0 and 1 to match
+draw(m_co2)
+
+#' *interpreting model summary*
+summary(m_co2)
+
+#' `Family: Gamma`: `y` is assumed to be *conditionally* Gamma-distributed
+#' `Formula`: model formula
+#' `Parametric coefficients`: table for intercept & slope coefficients
+#' `(Intercept)`: `co2_ppm` averaged across all smooth the functions and
+#'                setting all other parametric terms to 0
+#'  other coefficients: slopes of any parametric terms (i.e., not smooths)
+#' `s(year)`: the smooth effect of `year` for the average `s(season)`
+#' `ti(year,season)`: change in `s(year)` across `s(season)` and vice-versa
+#' 
+#' `edf`: effective degrees of freedom; measures the complexity of smooth
+#' `Ref.df`: reference degrees of freedom for F test
+#' `F`: F statistic for F test
+#' `p-value`: p-value from the F test
+#' *note:* (approximate) significance does not imply biological relevance
+#' 
+#' smooths are centered at 0 to show the deviations from the `(Intercept)`
+#' thus, if `year` & `season` are unknown, the best guess is the intercept
+#' 
+#' `p-values` for smooth terms are approximate and likely too small
+#' small changes in `k` can result in very different `p-values`
+#' `p-values` decrease quickly with sample size
+#' 
+#' `R-sq.(adj)` (adjusted R^2): only useful for Gaussian models
+#' `Deviance explained`: use as `R-sq.` for non-Gaussian models
+#' `Scale est.`: estimated scale parameter
+#' `n`: sample size
+
+#' *predicting with GLMs & GAMs*
+new_d <- tibble(year = seq(1950, 2000, length.out = 5),
+                season = 0) # January 1st
+
+# predictions on the link (log) scale
+predict(m_co2, newdata = new_d, type = 'link')
+
+# predictions on the response scale
+predict(m_co2, newdata = new_d, type = 'response')
+predict(m_co2, newdata = new_d, type = 'link') %>% exp()
+
+# also calculate standard error: returns a list
+predict(m_co2, newdata = new_d, type = 'link', se.fit = TRUE)
+
+#' only predict for specific terms by specifying `terms` argument
+#' e.g., `terms = 's(year)'`:
+#' - excludes `(Intercept)`
+#' - sets `s(season) = ti(year,season) = 0`
+#' - returns the relative change over `year`s, centered at log(1) = 0
+predict(m_co2, newdata = new_d, type = 'link', terms = 's(year)')
+predict(m_co2, newdata = new_d, type = 'response', terms = 's(year)')
+
+#' exclude specific terms by specifying `exclude` argument
+#' e.g., `exclude = 's(year)'`:
+#' - excludes `(Intercept)`
+#' - sets `s(season) = ti(year,season) = 0`
+predict(m_co2, newdata = new_d, type = 'link',
+        exclude = c('(Intercept)', 's(season)', 'ti(year,season)'))
+
+#' *plotting model terms*
+# relative change over the years
+tibble(year = seq(1959, 1997, length.out = 400),
+       season = 0) %>% # value is irrelevant because the smooth is excluded
+  mutate(rel_change = predict(m_co2, newdata = ., type = 'response',
+                              terms = 's(year)')) %>%
+  ggplot() +
+  geom_line(aes(year, rel_change), linewidth = 1) +
+  geom_hline(yintercept = exp(0), lty = 'dashed') + # reference line
+  labs(x = 'Year CE',
+       y = expression(Relative~change~'in'~CO[2]~concentration~(ppm)))
+
+# change over the years, ignoring seasonal trends
+tibble(year = seq(1959, 1997, length.out = 400),
+       season = 0) %>% # value is irrelevant because the smooth is excluded
+  mutate(mu_hat = predict(m_co2, newdata = ., type = 'response',
+                          terms = c('s(year)', '(Intercept)'))) %>%
+  ggplot() +
+  geom_line(aes(year, mu_hat), linewidth = 1) +
+  geom_hline(yintercept = exp(coef(m_co2)['(Intercept)']), lty = 'dashed')+
+  labs(x = 'Year CE',
+       y = expression(CO[2]~concentration~(ppm)))
+
+# change over the years, ignoring seasonal trends
+preds_full <- tibble(year = seq(1959, 1997 - 0.001, by = 0.001),
+                     season = year - floor(year)) %>%
+  mutate(mu_hat = predict(m_co2, newdata = ., type = 'response'))
+
+# line plot
+ggplot(preds_full) +
+  geom_line(aes(year, mu_hat), linewidth = 1) +
+  geom_hline(yintercept = exp(coef(m_co2)['(Intercept)']), lty = 'dashed')+
+  labs(x = 'Year CE', y = expression(CO[2]~concentration~(ppm)))
+
+# group by season
+ggplot(preds_full) +
+  geom_line(aes(year, mu_hat, color = season * 365, group = season),
+            linewidth = 1) +
+  geom_hline(yintercept = exp(coef(m_co2)['(Intercept)']), lty = 'dashed')+
+  labs(x = 'Year CE', y = expression(CO[2]~concentration~(ppm))) +
+  scale_color_gradientn(name = 'Day of year',
+                        colors = c(color('batlow')(10),
+                                   color('batlow', reverse = TRUE)(10)))
+
+# surface plot by year and day of year
+ggplot(preds_full) +
+  geom_raster(aes(season * 365, floor(year), fill = mu_hat)) +
+  scale_x_continuous('Day of year', expand = c(0, 0)) +
+  scale_y_continuous('Year CE', expand = c(0, 0)) +
+  scale_fill_lipari(name = expression(Mean~CO[2]~(ppm)))
+
+#' add confidence intervals using `gratia::fitted_values()`
+new_d_year <- tibble(year = seq(1959, 1997, by = 0.001), season = 0)
+fitted_values(m_co2, data = new_d_year)
+
+# add confidence intervals manually
+dof <- m_co2$df.residual
+preds_year_ci <-
+  bind_cols(new_d_year,
+            predict(m_co2, new_d_year, type = 'link', se.fit = TRUE,
+                    exclude = c('s(season)', 'ti(year,season)')) %>%
+              as.data.frame()) %>%
+  mutate(mu_hat = exp(fit),
+         lower_95_ci = exp(fit + qt(0.0005, df = dof) * se.fit), # 99.9% CI
+         upper_95_ci = exp(fit + qt(0.9995, df = dof) * se.fit))
+preds_year_ci
+
+p_year <-
+  ggplot(preds_year_ci) +
+  geom_ribbon(aes(year, ymin = lower_95_ci, ymax = upper_95_ci),
+              alpha = 0.3) +
+  geom_line(aes(year, mu_hat)) +
+  labs(x = 'Year CE', y = expression(CO[2]~concentration~(ppm)))
+p_year
+
+#' *interpreting credible intervals produced by mgcv*
+
+
+#' *creating publication-level figures*
+p_doy <-
+  tibble(year = 1959, # excluded below
+         season = seq(0, 1, length.out = 400),
+         doy = season * 365) %>%
+  bind_cols(.,
+            predict(m_co2, ., type = 'link', se.fit = TRUE,
+                    exclude = c('s(year)', 'ti(year,season)')) %>%
+              as.data.frame()) %>%
+  mutate(mu_hat = exp(fit),
+         lower_95_ci = exp(fit + qt(0.0005, df = dof) * se.fit), # 99.9% CI
+         upper_95_ci = exp(fit + qt(0.9995, df = dof) * se.fit)) %>%
+  ggplot() +
+  geom_ribbon(aes(doy, ymin = lower_95_ci, ymax = upper_95_ci),
+              alpha = 0.3) +
+  geom_line(aes(doy, mu_hat)) +
+  scale_x_continuous('Day of year', expand = c(0, 0)) +
+  ylab(expression(CO[2]~concentration~(ppm)))
+p_doy
+
+p_ti <-
+  expand_grid(year = seq(1959, 1997, length.out = 400),
+              season = seq(0, 1, length.out = 400)) %>%
+  mutate(doy = season * 365) %>%
+  mutate(rel_change = predict(m_co2, ., type = 'response', se.fit = FALSE,
+                    terms = c('(Intercept)', 'ti(year,season)')) %>%
+              as.numeric(), #' `predict()` returns array: make vector
+         percent_change = (rel_change - exp(coef(m_co2)['(Intercept)'])) * 100) %>%
+  ggplot() +
+  geom_raster(aes(doy, year, fill = percent_change)) +
+  scale_x_continuous('Day of year', expand = c(0, 0)) +
+  scale_y_continuous('Year CE', expand = c(0, 0)) +
+  scale_fill_vik(name = expression(atop(Change~'in'~seasonal~trend,
+                                        'in'~CO[2]~concentration~(ppm))),
+                 midpoint = 0, limits = c(-8, 8)) +
+  theme(legend.position = 'top', legend.key.width = rel(2))
+p_ti
+
+# add all plots together
+plot_grid(plot_grid(p_year, p_doy, labels = 'AUTO', nrow = 1),
+          p_ti, labels = c('', 'C'), ncol = 1)
+
+#' *deciding sampling frequency before data collection*
+
