b63669251ba5fc8e991852df5e8d7a8552471f78
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-11-22 15:01:44 -0600

    added section notes for day 5

---
+++ day-5/day-5.R
@@ -0,0 +1,17 @@
+#' Day 5: *Hierarchical Generalized Additive Models*
+#' attach necessary packages
+theme_set(theme_bw())
+
+#' modeling data from multiple sites/individuals,
+#' random vs fixed effects
+#' modeling large amounts of data.
+#'
+#' **break** --------------------------------------------------------------
+#' 
+#' experimental design and inference
+#' BACI design
+#' control and treatment sites
+#' `by` binary smooths
+#' interpreting random vs fixed effects
+#' predicting out of sample (random effects)
+#' forecasting (leveraging hierarchical structure)
